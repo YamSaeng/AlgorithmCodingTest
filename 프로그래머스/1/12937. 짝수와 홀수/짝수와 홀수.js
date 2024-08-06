7b056818d8d6a55fa7713a9be9df43bdb9cb3645
YamSaeng <38998485+YamSaeng@users.noreply.github.com>
2024-08-06 16:13:17 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    var answer = 0;
+    
+    
+    if(num % 2 == 0)
+    {
+        answer = "Even";
+    }
+    else
+    {
+        answer = "Odd";
+    }
+    
+    return answer;
+}
