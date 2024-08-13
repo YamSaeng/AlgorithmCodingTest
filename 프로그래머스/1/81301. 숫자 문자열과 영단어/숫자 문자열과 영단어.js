6d3697df7f8e541f1ec3a26f6cb700e9cd5e283c
YamSaeng <38998485+YamSaeng@users.noreply.github.com>
2024-08-13 13:15:39 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,28 +1,27 @@
 function solution(s) {
-    var answer = 0;
+    var answer = [];
 
     let numbers = new Map();
     numbers.set("zero", 0); numbers.set("one", 1); numbers.set("two", 2); numbers.set("three", 3); numbers.set("four", 4);
     numbers.set("five", 5); numbers.set("six", 6); numbers.set("seven", 7); numbers.set("eight", 8); numbers.set("nine", 9);
 
-    let TempNumberString = [];
-    let TempNumber = [];
+    let NumberString = [];
+
     for (let i = 0; i < s.length; i++) {
-        TempNumberString.push(s[i]);
-        let TempNumberStringKey = TempNumberString.join("");
+        NumberString.push(s[i]);
+
+        let NumberStringKey = NumberString.join("");
         
-        if(isNaN(parseInt(TempNumberStringKey)) != true)
+        if(isNaN(parseInt(NumberStringKey)) != true)
         {
-            TempNumber.push(parseInt(TempNumberStringKey));
-            TempNumberString = [];            
+            answer.push(parseInt(NumberStringKey));
+            NumberString = [];            
         }
-        else if (numbers.get(TempNumberStringKey) != null) {
-            TempNumber.push(numbers.get(TempNumberStringKey));
-            TempNumberString = [];
+        else if (numbers.get(NumberStringKey) != null) {
+            answer.push(numbers.get(NumberStringKey));
+            NumberString = [];
         }        
-    }
+    }   
 
-    answer = parseInt(TempNumber.join(""));
-
-    return answer;
+    return parseInt(answer.join(""));
 }
