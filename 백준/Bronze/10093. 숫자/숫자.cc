4b69e8a6bef0a404b248d9439453bd2db13225c4
YamSaeng <38998485+YamSaeng@users.noreply.github.com>
2024-08-26 19:23:50 +0900

    [Bronze II] Title: 숫자, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -6,15 +6,13 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int a, b;
+	long long a, b;
 
 	cin >> a >> b;
 	
 	if (a > b)
 	{
-		int c = a;
-		a = b;
-		b = c;
+		swap(a, b);
 	}
 
 	if (a == b || b - a == 1)
