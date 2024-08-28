767bf286e00f5b5241f85483efbb53273bffd5da
YamSaeng <38998485+YamSaeng@users.noreply.github.com>
2024-08-28 12:21:00 +0900

    [level 1] Title: 과일 장수, Time: 213.22 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,11 @@
+function solution(k, m, score) {
+    var answer = 0;
+
+    score.sort((a, b) => a - b);
+
+    for (let i = score.length - m; i >= 0; i -= m) {
+        answer += score[i] * m;
+    }
+
+    return answer;
+}
