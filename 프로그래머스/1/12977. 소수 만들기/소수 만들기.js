ede3488b7ae71ebc15fc06eabda55fd80b9ccc3a
YamSaeng <38998485+YamSaeng@users.noreply.github.com>
2024-08-31 12:40:47 +0900

    [level 1] Title: 소수 만들기, Time: 15.37 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,8 +1,28 @@
 function solution(nums) {
-    var answer = -1;
-    
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-    
+    var answer = 0;
+
+    for (let i = 0; i < nums.length; i++)
+    {
+        for (let j = i + 1; j < nums.length; j++)
+        {
+            for (let k = j + 1; k < nums.length; k++)
+            {
+                let sum = nums[i] + nums[j] + nums[k];
+                let isPrime = true;
+
+                for (let l = 2; l < sum / 2; l++) {
+                    if (sum % l == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime == true) {
+                    answer++;
+                }
+            }
+        }
+    }
+
     return answer;
 }
